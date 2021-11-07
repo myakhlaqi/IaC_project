a8017912f2e455e7b4d0f8dd692bd0642562d4d4
yahya <csmaster90@hotmail.com>
2021-11-07 23:17:51 +0800

    Unzip the production project files

---
+++ Environment/production/modules/packages/manifests/init.pp
@@ -0,0 +1,29 @@
+class packages {
+
+   package { 'python-requests':
+
+       ensure => installed,
+
+   }
+
+   if $facts[os][family] == "Debian" {
+
+     package { 'golang':
+
+       ensure => installed,
+
+     }
+
+  }
+
+   if $facts[os][family] == "RedHat" {
+
+     package { 'nodejs':
+
+       ensure => installed,
+
+     }
+
+  }
+
+}
