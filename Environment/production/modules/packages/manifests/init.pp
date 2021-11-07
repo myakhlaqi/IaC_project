2748910928f0009738e667961c307df5a6725f82
Mohammad Yahya Akhlaqi <91276692+myakhlaqi@users.noreply.github.com>
2021-11-07 23:20:08 +0800

    Update init.pp

--- Environment/production/modules/packages/manifests/init.pp
+++ Environment/production/modules/packages/manifests/init.pp
@@ -1,25 +1,14 @@
 class packages {
-
    package { 'python-requests':
-
        ensure => installed,
-
    }
-
    if $facts[os][family] == "Debian" {
-
      package { 'golang':
-
        ensure => installed,
-
      }
-
   }
-
    if $facts[os][family] == "RedHat" {
-
      package { 'nodejs':
-
        ensure => installed,
 
      }
